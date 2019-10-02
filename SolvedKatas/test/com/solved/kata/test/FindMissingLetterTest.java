d0896d27a2d6a4a95567f60db4908d075580cb5c
DanielGiraldo <danielalberto.giraldo.contractor@bbva.com>
2019-10-02 17:48:06 -0500

    FindMissingLetterKata

---
+++ SolvedKatas/test/com/solved/kata/test/FindMissingLetterTest.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.solved.kata.test;
+
+import com.solved.kata.FindMissingLetter;
+
+/**
+ *
+ * @author dgiraldo
+ */
+public class FindMissingLetterTest {
+
+    public static void main(String[] args) {
+        System.out.println(FindMissingLetter.findMissingLetter(new char[]{'a', 'b', 'c', 'd',
+                'e', 'f', 'g', 'h',
+                'i', 'j', 'k', 'l',
+                'm', 'n', 'o', 'p',
+                'q', 'r', 's', 't',
+                'u', 'v', 'w', 'x',
+                'y', 'z', 'a', 'b',
+                'c', 'd', 'f'}));
+    }
+}
