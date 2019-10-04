136d05dc0f9da283c1cee4b1d56a3f93fdcb9cba
DanielGiraldo <danielalberto.giraldo.contractor@bbva.com>
2019-10-04 17:09:58 -0500

    AlphabeticalAdition Kata

---
+++ SolvedKatas/test/com/solved/kata/test/AlphabeticalAditionTest.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.solved.kata.test;
+
+import com.solved.kata.AlphabeticalAdition;
+import static org.junit.Assert.assertEquals;
+import org.junit.Test;
+
+/**
+ *
+ * @author dgiraldo
+ */
+public class AlphabeticalAditionTest {
+
+    @Test
+    public void fixedTests() {
+        assertEquals("f", AlphabeticalAdition.addLetters("a", "b", "c"));
+        assertEquals("z", AlphabeticalAdition.addLetters("z"));
+        assertEquals("c", AlphabeticalAdition.addLetters("a", "b"));
+        assertEquals("c", AlphabeticalAdition.addLetters("c"));
+        assertEquals("a", AlphabeticalAdition.addLetters("z", "a"));
+        assertEquals("d", AlphabeticalAdition.addLetters("y", "c", "b"));
+        assertEquals("z", AlphabeticalAdition.addLetters());
+    }
+}
